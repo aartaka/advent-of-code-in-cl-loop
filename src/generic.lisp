166ac91232ce774b28e3d6cc8b52fb8c1aa0077e
Artyom Bologov <aartaka@protonmail.com>
2021-12-05 23:22:39 +0500

    generic: Add docstrings to all the loop-reimplementations.

--- src/generic.lisp
+++ src/generic.lisp
@@ -12,11 +12,13 @@
         :collect (funcall function l)))
 
 (defun loopind (item list)
+  "A loop-based copy of `find'."
   (loop :for e :in list
         :when (equal item e)
           :do (return t)
         :finally (return nil)))
 
 (defun loopverse (list)
+  "A loop-based copy of `reverse'."
   (loop :for i :from (1- (length list)) :downto 0
         :collect (elt list i)))
