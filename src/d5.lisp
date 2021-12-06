7d5ffa28f425f84b36c9bc7265f2e5b8169a77aa
Artyom Bologov <aartaka@protonmail.com>
2021-12-06 17:02:42 +0500

    d5: TIL and refactoring.

--- src/d5.lisp
+++ src/d5.lisp
@@ -20,29 +20,34 @@
   (= (second (first line))
      (second (second line))))
 
-(defun d5-1 ()
+(defun direction (number)
+  (cond
+    ((plusp number) 1)
+    ((zerop number) 0)
+    ((minusp number) -1)))
+
+(export 'd5-2)
+(defun d5-2 ()
+  "TIL that:
+- CL:LOOP has no generic arithmetic stepping (e.g., to work both from -5 to 5 and from 5 to -5).
+  - To have this generic stepping, a form like `FOR X = X0 THEN (INCF X BY)' would work,
+    but this is essentially a C-like loop :/
+- Off-by-one errors are indeed dreadful.
+
+How do I solve this without a two-dimentional array?"
   (loop with lines = (lines)
-        with x-dimension = (1+ (loop for ((x1 y1) (x2 y2)) in (aoc::lines)
+        with x-dimension = (1+ (loop for ((x1 __) (x2 _)) in lines
                                      maximize (max x1 x2)))
-        with y-dimension = (1+ (loop for ((x1 y1) (x2 y2)) in (aoc::lines)
+        with y-dimension = (1+ (loop for ((x1 y1) (x2 y2)) in lines
                                      maximize (max y1 y2)))
         with map = (make-array (list x-dimension y-dimension) :initial-element 0)
-        with tmp
+        with fill-map
           = (loop for ((x1 y1) (x2 y2)) in lines
-                  for line in lines
-                  do (loop with xs = (loop for x from (min x1 x2) to (max x2 x1)
-                                           collect x into xs
-                                           finally (return (if (> x1 x2) (loopverse xs) xs)))
-                           with ys = (loop for y from (min y1 y2) to (max y2 y1)
-                                           collect y into ys
-                                           finally (return (if (> y1 y2) (loopverse ys) ys)))
-                           for x in (if (= (length xs) 1)
-                                        (make-list (length ys) :initial-element (first xs))
-                                        xs)
-                           for y in (if (= (length ys) 1)
-                                        (make-list (length xs) :initial-element (first ys))
-                                        ys)
-                           do (incf (aref map x y))))
+                  do (loop for x = x1 then (incf x (direction (- x2 x1)))
+                           for y = y1 then (incf y (direction (- y2 y1)))
+                           until (and (= x x2) (= y y2))
+                           do (incf (aref map x y))
+                           finally (incf (aref map x y))))
         for x from 0 below (array-dimension map 0)
         sum (loop for y from 0 below (array-dimension map 1)
                   count (> (aref map x y) 1))))
