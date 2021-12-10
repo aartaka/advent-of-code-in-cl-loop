900544affa9aa2ed5cdc69c1855f2f228451a083
Artyom Bologov <aartaka@protonmail.com>
2021-12-10 18:07:05 +0500

    generic: Loop keywords as symbols.

--- src/generic.lisp
+++ src/generic.lisp
@@ -2,29 +2,29 @@
 
 (defun loopmove-if (predicate list)
   "A loop-based copy of `remove-if'."
-  (loop :for l :in list
-        :unless (funcall predicate l)
-          :collect l))
+  (loop for l in list
+        unless (funcall predicate l)
+          collect l))
 
 (defun loopmove (item list)
   (loopmove-if (lambda (i) (equal i item)) list))
 
 (defun loopcar (function list)
   "A loop-based copy of `mapcar'."
-  (loop :for l :in list
-        :collect (funcall function l)))
+  (loop for l in list
+        collect (funcall function l)))
 
 (defun loopind (item list)
   "A loop-based copy of `find'."
-  (loop :for e :in list
-        :when (equal item e)
-          :do (return t)
-        :finally (return nil)))
+  (loop for e in list
+        when (equal item e)
+          do (return t)
+        finally (return nil)))
 
 (defun loopverse (list)
   "A loop-based copy of `reverse'."
-  (loop :for i :from (1- (length list)) :downto 0
-        :collect (elt list i)))
+  (loop for i from (1- (length list)) downto 0
+        collect (elt list i)))
 
 (defun loop-diff (l1 l2)
   "A loop-based copy of `set-difference'."
