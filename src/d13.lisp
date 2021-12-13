a9c96a987a7ae561830aa4abb31a1686a649b375
aartaka <aartaka@protonmail.com>
2021-12-13 15:31:59 +0500

    d13: Part two.

--- src/d13.lisp
+++ src/d13.lisp
@@ -34,6 +34,30 @@
                collect (list (- x-fold (- x-dot x-fold)) y-dot)
         else collect (list x-dot y-dot)))
 
+(export 'd13-1)
 (defun d13-1 ()
   (loop with (dots instructions) = (folding)
         initially (return (loopmove-duplicates (fold dots (first instructions))))))
+
+(defun print-paper (paper)
+  (loop with (max-x max-y) = (loop for (x y) in paper
+                                   maximize x into max-x
+                                   maximize y into max-y
+                                   finally (return (list max-x max-y)))
+        with strings = (loop for y upto max-y
+                             collect (make-string (1+ max-x) :initial-element #\ ))
+        for (x y) in paper
+        do (setf (elt (elt strings y) x)
+                 #\#)
+        finally (print (loop with result = ""
+                              for string in strings
+                              do (setf result (concatenate 'string result (string #\Newline) string))
+                              finally (return result)))))
+
+(export 'd13-2)
+(defun d13-2 ()
+  (loop with (dots instructions) = (folding)
+        with fold = dots
+        for instruction in instructions
+        do (setf fold (loopmove-duplicates (fold fold instruction)))
+        finally (print-paper fold)))
